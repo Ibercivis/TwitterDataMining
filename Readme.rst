14a9e8f6ebd069a8997b23059fc7cf59cdfb709c
XayOn <opensource@davidfrancos.net>
2011-05-19 15:11:29 +0200

    foobar

--- Readme.rst
+++ Readme.rst
@@ -6,7 +6,24 @@ Despues crea una plantilla de pegatinas con esos tweets, directas para imprimir.
 
 ¡Expande tus pensamientos IRL!
 
+Uso
+====
+git clone https://github.couuuuuOn/RealLifeTwitter.git
+cd RealLifeTwitter/
+python RealLifeTweeter.py --hashtag notenemosmiedo --user tomalaplaza --destfile notenemosmiedo.html --timeout 60
+> Cuando termine, pulsa "f"
+firefox notenemosmiedo_tomalaplaza.html
+
 Real life twitter
 =================
 This project aims to get the most important tweets about a subject, a group or subjects, a person or group of persons (even mixed!).
 Then it'll create stickers, allowing you to "Real Life Tweet", and spread your tweet-toughs in real world.
+
+
+Usage
+======
+git clone https://github.com/XayOn/RealLifeTwitter.git
+cd RealLifeTwitter/
+python RealLifeTweeter.py --hashtag notenemosmiedo --user tomalaplaza --destfile notenemosmiedo.html --timeout 60
+> Then press "f"
+firefox notenemosmiedo_tomalaplaza.html
--- RealLifeTweeter.py
+++ RealLifeTweeter.py
@@ -3,7 +3,7 @@
 import argparse
 import time 
 from RLT import *
-
+import time
 class main(tweetWeightParser, InterchangeableInterface, ResultsGenerator):
     def __init__(self):
         super(self.__class__, self).__init__()
@@ -12,14 +12,18 @@ class main(tweetWeightParser, InterchangeableInterface, ResultsGenerator):
         self.parser.add_argument('--hashtag', dest='hashtag',  help='hashtag list to search in')
         self.parser.add_argument('--destfile', dest='file',  help='hashtag list to search ina')
         self.parser.add_argument('--filter', dest='filter_',  help='filter user-driven search into a specific word')
+        self.parser.add_argument('--timeout', dest='timeout',  help='End the bucle after X seconds')
         self.args = self.parser.parse_args()
         self.finished=False
         self.exit=False
         self.warn("Processing your petitions, press ctrl+C to stop anytime")
         if not type(self.args.hashtag) is list: self.args.hashtag=[self.args.hashtag]
         if not type(self.args.user) is list: self.args.user=[self.args.user]
-
+        self.start_time=time.time()
         while not self.finished:
+            spent_time=int(time.time() - self.start_time)
+            if self.args.timeout and ( time.time() - self.start_time ) > int(self.args.timeout) : break
+
             try:
                 self.get_by_hashtag(self.args.hashtag)
                 time.sleep(10)
