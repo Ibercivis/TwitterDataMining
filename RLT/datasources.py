f8f5b1f5f2d6c404cb1eeac83405dade3969bc8c
XayOn <opensource@davidfrancos.net>
2011-06-08 10:11:51 +0200

    Another exception catching

--- RLT/datasources.py
+++ RLT/datasources.py
@@ -28,9 +28,12 @@ class twitterParser(object):
                 user=self.api.GetUser(user.strip())
                 self.users.append( { user.id : [ user.AsDict(), self.api.GetFollowerIDs(user.id) , self.api.GetFriendIDs(user.id) ]} )
             except Exception, e:
-                import time
-                print "Exception: %s\nSleeping an hour" %e
-                time.sleep(3600)
+                if "Limit" in e:
+                    import time
+                    print "Exception: %s\nSleeping an hour" %e
+                    time.sleep(3600)
+                else:
+                    print "Exception: %s" %e
 
     def get_by_timeline_array(self, timelines):
         filter_=self.args.filter_
