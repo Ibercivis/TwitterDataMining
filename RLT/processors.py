168890cc308eaa5975befed54b9b9d46f25881fe
XayOn <opensource@davidfrancos.net>
2011-06-24 15:10:16 +0200

    foo

--- RLT/processors.py
+++ RLT/processors.py
@@ -59,6 +59,7 @@ class MYSQLExporter(Result):
         if not self.args.mysql: return
         import MySQLdb
         if len(obj[0] > 1):
+            print "Getting user data"
             aq= "update tw_user set tw_id=\'%s\', task_status=\'%s\', task_host=\'%s\', created_at=\'%s\', statuses_count=\'%s\', friend_count=\'%s\', followers_count=\'%s\', geo_lat=\'%s\', geo_long=\'%s\', geo_text=\'%s\' where  name=\'%s\'" ,
         else:
             aq="update tw_user set tw_id=\'%s\', task_status=1 where name=\'%s\'"
